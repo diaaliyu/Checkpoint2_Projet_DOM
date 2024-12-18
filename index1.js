8dad6731475bdc27c34d8d0607c6496444d9f2d4
diaaliyu <diaseyudunaaliyu@gmail.com>
2024-12-18 18:47:57 +0000

    Projet DOM 2

---
+++ index1.js
@@ -0,0 +1,8 @@
+const generateColor = () => {
+  
+    let randomColor = (Math.floor(Math.random()*0xFFFFFF)).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    let text = document.querySelector('#code');
+    text.innerText = "#" + randomColor;
+  
+}
